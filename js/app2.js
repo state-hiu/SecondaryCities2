9d2aa512df195644a5fd4f67bd9c130f1158c03d
d3netxer <tommygun320@yahoo.com>
2016-07-07 22:41:53 -0400

    adding Pokhara GeoNode link to main site, and Kharkiv story to Kharkiv city page

--- js/app2.js
+++ js/app2.js
@@ -121,7 +121,7 @@ $(document).foundation();
             $( "#sidebar" ).hide();
             $( "#map" ).hide();
             //insert esri story map iframe
-            $( "body").append( '<iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="margin-top:74px;" src="http://csurams.maps.arcgis.com/apps/MapJournal/index.html?appid=4e3e6e624fd048718066294207008a75"></iframe>');
+            $( "body").append( '<iframe width="100%" height="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="margin-top:74px;" src="http://csurams.maps.arcgis.com/apps/MapJournal/index.html?appid=f06c1ad728e4447c841f1def56ae3c91"></iframe>');
             flag=1;
             return;
           }
