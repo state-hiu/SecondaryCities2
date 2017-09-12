63baaa318e1011f07b73cb541327d674546d8bec
d3netxer <tommygun320@yahoo.com>
2017-09-11 22:48:37 -0400

    improving mobile

--- assets/js/libs/freelancer.js
+++ assets/js/libs/freelancer.js
@@ -5,7 +5,21 @@
  */
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
+
+//on mobile devices it was over-scrolling when clicking on menu items for anchor links
+//This is probably because the responsive menu push the items in the page down
+if (window.innerWidth < 400) {
+    $(function() {
+    $('body').on('click', '.page-scroll a', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 0, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+} else {
+    $(function() {
     $('body').on('click', '.page-scroll a', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
@@ -14,6 +28,8 @@ $(function() {
         event.preventDefault();
     });
 });
+}
+
 
 // Floating label headings for the contact form
 $(function() {
